3c482e452999e941ca6127735f786797482ab5a2
Andreas Rasmussen <andreasrefras@gmail.com>
2015-07-02 11:00:18 +0200

    Bigger blockquote under 'Om'

--- page-om.php
+++ page-om.php
@@ -35,7 +35,7 @@
           </div>
         </article>
 
-           <blockquote class="hvadhvof-blockquote col-xs-9 col-xs-offset-2 col-sm-8 col-sm-offset-3 col-md-12 col-md-offset-2">
+           <blockquote class="hvadhvof-blockquote col-xs-9 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-7 col-md-offset-2">
               <p>Ledelse er at skabe følgeskab - på den gode måde.
               Det vil sige give mod, styrke... og visdom.</p>
               <cite>- Lukas, 12 år</cite>
